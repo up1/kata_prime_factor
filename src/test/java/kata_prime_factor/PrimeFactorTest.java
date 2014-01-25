c4f5e48c5ee2fb9dfd200b003bb02adee7661172
Somkiat <somkiat.p@gmail.com>
2014-01-25 22:25:47 +0700

    Add Test 2 and update list method

--- src/test/java/kata_prime_factor/PrimeFactorTest.java
+++ src/test/java/kata_prime_factor/PrimeFactorTest.java
@@ -2,6 +2,7 @@ package kata_prime_factor;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -13,7 +14,17 @@ public class PrimeFactorTest {
 		assertEquals(list(), primeFactor.generate(1));
 	}
 	
-	private List<Integer> list() {
-		return null;
+	@Test
+	public void testTwo() throws Exception {
+		PrimeFactor primeFactor = new PrimeFactor();
+		assertEquals(list(2), primeFactor.generate(2));
+	}
+	
+	private List<Integer> list(int... integers) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int integer : integers) {
+			list.add(integer);
+		}
+		return list;
 	}
 }
