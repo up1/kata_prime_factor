a13349f35158a41473ee475a8f1e54e667a943ed
Somkiat <somkiat.p@gmail.com>
2014-01-25 22:42:27 +0700

    Test 4 : create code to pass by using more if statement

--- src/main/java/kata_prime_factor/PrimeFactor.java
+++ src/main/java/kata_prime_factor/PrimeFactor.java
@@ -7,8 +7,14 @@ public class PrimeFactor {
 
 	public List<Integer> generate(int integer) {
 		List<Integer> primes = new ArrayList<Integer>();
-		if(integer>1) {
-			primes.add(integer);
+		if (integer > 1) {
+			if (integer % 2 == 0) {
+				primes.add(2);
+				integer = integer / 2;
+			}
+			if (integer > 1) {
+				primes.add(integer);
+			}
 		}
 		return primes;
 	}
