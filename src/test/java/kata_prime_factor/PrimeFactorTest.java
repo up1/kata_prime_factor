a3d74b55e90ed236db43b3c231173ece0f734bde
Somkiat <somkiat.p@gmail.com>
2014-01-25 22:17:13 +0700

    Test 1 : prime factor of 1 = null

--- src/test/java/kata_prime_factor/PrimeFactorTest.java
+++ src/test/java/kata_prime_factor/PrimeFactorTest.java
@@ -1,5 +1,19 @@
 package kata_prime_factor;
 
-public class PrimeFactorTest {
+import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Test;
+
+public class PrimeFactorTest {
+	@Test
+	public void testOne() throws Exception {
+		PrimeFactor primeFactor = new PrimeFactor();
+		assertEquals(list{}, primeFactor.generate(1));
+	}
+	
+	private List<Integer> list() {
+		return null;
+	}
 }
