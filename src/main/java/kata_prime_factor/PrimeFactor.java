1dcc6c9e5bd5ac09451a7e51a70259bf4bf1ce0c
Somkiat <somkiat.p@gmail.com>
2014-01-25 22:28:27 +0700

    Comment Test 2 because change code in list() method to return ArrayList<Integer>

--- src/main/java/kata_prime_factor/PrimeFactor.java
+++ src/main/java/kata_prime_factor/PrimeFactor.java
@@ -1,11 +1,12 @@
 package kata_prime_factor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactor {
 
 	public List<Integer> generate(int i) {
-		return null;
+		return new ArrayList<Integer>();
 	}
 
 }
